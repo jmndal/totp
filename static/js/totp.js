35c387d7fdc5207c8a88c5b908c2de42fba61285
Joan Marie Andal <joanmarieandal@gmail.com>
2023-04-12 14:24:53 +0800

    MOD: Function for generating key

--- static/js/totp.js
+++ static/js/totp.js
@@ -1,19 +1,28 @@
 
 
 function generateKey() {
-    $("#key-input").text(generateString(15))
+  $.ajax({
+    method: "POST",
+    url: "/",
+    data: {
+      data_action: "GENERATE",
+    },
+    success: function () {
+      console.log("KEY GENERATED")
+    },
+  });
 }
 
-const characters =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+// const characters =
+//   "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-function generateString(length) {
-  let result = "";
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
+// function generateString(length) {
+//   let result = "";
+//   const charactersLength = characters.length;
+//   for (let i = 0; i < length; i++) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
