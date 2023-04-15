1542a6006507ffdabaa958e2000bd4e4387a8e82
Revelyn Enriquez <enriquezrevs@gmail.com>
2023-04-15 14:29:18 +0800

    add validation for OTP and timer

--- static/js/totp.js
+++ static/js/totp.js
@@ -24,7 +24,36 @@ function generateTOTP() {
     },
     success: function () {
       // $("#key-input").text(result);
+    
+      console.log("hi")
+     
+
     },
   });
 }
 
+function validateOTP() {
+  // Send a POST request to the server to validate the OTP
+  console.log("test", $("#otp-input").text() == $("#totp").val())
+  if ($("#otp-input").text() == $("#totp").val()) {
+    $("#otp-status").text("TOTP code is valid!") 
+  } else {
+    $("#otp-status").text("Invalid TOTP code!") 
+  }
+}
+
+function updateTimer() {
+  var now = new Date();
+  var timeLeft = 30 - now.getSeconds() % 30;
+  $("#timer").text(timeLeft + ' seconds');
+
+  if (now.getSeconds() % 30 === 0) {
+    // Wait 1 second and reload the page
+    setTimeout(function () {
+      location.reload();
+    }, 5);
+  }
+}
+
+console.log("hi")
+setInterval(updateTimer, 1000);
